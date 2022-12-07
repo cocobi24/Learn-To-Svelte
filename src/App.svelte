d31930baa1b3f201768f6b3ca628b2a33aed667a
seonhui-lee <cacabi24@hanmail.net>
2022-12-07 23:43:29 +0900

    bind:value
    bind:value

--- src/App.svelte
+++ src/App.svelte
@@ -4,12 +4,18 @@
 
 	$: uppercaseName = name.toUpperCase();
 
+	$: age = name === "Hojin"? 19 : 29;
 	function incrementAge() {
 		age++;
 	}
 
 	function changeName() {
 		name = name === "Hojin"? "Jinny" : "Hojin";
+	}
+
+	function nameInput(event){
+		const enteredValue = event.target.value;
+		name = enteredValue;
 	}
 
 </script>
@@ -22,4 +28,6 @@
 
 <h1>Hello  My name is {uppercaseName}, my age is {age}!</h1>
 <button on:click="{incrementAge}">Change Age</button>
-<button on:click="{changeName}">Change Name</button>
+<!-- <button on:click="{changeName}">Change Name</button> -->
+<!-- <input type="text" value="{name}" on:input="{nameInput}"> -->
+<input type="text" bind:value="{name}">
