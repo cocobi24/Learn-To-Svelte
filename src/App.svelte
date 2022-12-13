868fd79fb9955f547cf877fac4f036469c04f67c
seonhui-lee <cacabi24@hanmail.net>
2022-12-13 20:40:58 +0900

    컴포넌트 바인딩(2)
    컴포넌트 바인딩(2)

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,9 @@
 	import ContactCard from "./ContactCard.svelte";
 
 	let name = 'Jinny'
+	let title = "" 
+	let image = "" 
+	let description = ""
 	let age = 29
 
 	$: uppercaseName = name.toUpperCase();
@@ -33,5 +36,13 @@
 <!-- <button on:click="{changeName}">Change Name</button> -->
 <!-- <input type="text" value="{name}" on:input="{nameInput}"> -->
 <input type="text" bind:value="{name}">
+<input type="text" bind:value="{title}">
+<input type="text" bind:value="{image}">
+<textarea rows="3" bind:value="{description}"></textarea>
 
-<ContactCard userName="{name}"/>
+  <ContactCard 
+	userName="{name}" 
+	jobTitle="{title}" 
+	description="{description}" 
+	userImage={image}
+/>
