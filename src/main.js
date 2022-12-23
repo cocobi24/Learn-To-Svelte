45610479ae1184a182cc01186c8a391c7aa4e3ac
seonhui-lee <cacabi24@hanmail.net>
2022-12-23 14:11:22 +0900

    컴포넌트 마운트 원복(SPA)
    컴포넌트 마운트 원복(SPA)

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import App from './App.svelte';
-import Header from './UI/Header.svelte'
+// import Header from './UI/Header.svelte'
 
 const app = new App({
-	target: document.querySelector('#app')
+	// target: document.querySelector('#app')
+	target: document.body
 });
 
-const header = new Header({
-	target: document.querySelector('#header')
-});
+// const header = new Header({
+// 	target: document.querySelector('#header')
+// });
 
 export default app;
