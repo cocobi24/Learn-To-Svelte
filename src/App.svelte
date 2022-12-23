1d5d632af3fb935fa8ec85642f870596470005f6
seonhui-lee <cacabi24@hanmail.net>
2022-12-23 14:19:32 +0900

    ✔ meetups 데이터 추가
    ✔ meetups 데이터 추가

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,26 @@
 <script>
   import Header from "./UI/Header.svelte";
+
+  const meetups = [
+    {
+      id: 'm1',
+      title: 'Coding Bootcamp',
+      subtitle: 'Learn to code in 2 hours',
+      description: 'In this meetup, we will have some ecperts that teach you how to code!',
+      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQM8zOzPYn4MZgzzWKkMzCNyBvQjk5DI5b39YaVaogXNg&s',
+      address: '27th Nerd Road, 32523 New York',
+      contactEmail: 'code@test.com'
+    },
+    {
+      id: 'm2',
+      title: 'Swim Together',
+      subtitle: 'Let\'s go for some swimming',
+      description: 'We will simply swim some rounds!',
+      imageUrl: 'https://www.history.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTY4OTA4MzI0ODc4NjkwMDAw/christmas-tree-gettyimages-1072744106.jpg',
+      address: '27th Nerd Road, 32523 New York',
+      contactEmail: 'swim@test.com'
+    }
+  ]
 </script>
 
 <Header />
