1b23f700f4d4f5a14c94b5b1dcbd166e4357d475
LEEHOJIN <cacabi24@hanmail.net>
2023-01-21 13:19:30 +0900

    ✔ MeetupItem에 transition 적용
    ✔ MeetupItem에 transition 적용

--- src/Meetups/MeetupGrid.svelte
+++ src/Meetups/MeetupGrid.svelte
@@ -1,5 +1,7 @@
 <script>
   import { createEventDispatcher } from "svelte";
+  import { scale } from "svelte/transition";
+  import { flip } from "svelte/animate";
   import MeetupItem from "./MeetupItem.svelte";
   import MeetupFilter from "./MeetupFilter.svelte";
   import Button from "../UI/Button.svelte";
@@ -42,8 +44,9 @@
   <Button on:click={() => dispatch('add')}>New Meetup</Button>
 </section>
 <section id="meetups">
-  {#each filteredMeetups as meetup}
-    <MeetupItem
+  {#each filteredMeetups as meetup (meetup.id)}
+    <div transition:scale animate:flip={{duration: 300}}>
+      <MeetupItem
       id={meetup.id}
       title={meetup.title}
       subtitle={meetup.subtitle}
@@ -54,5 +57,6 @@
       isFav={meetup.isFavorite}
       on:showdetails
       on:edit />
+    </div>
   {/each}
 </section>
